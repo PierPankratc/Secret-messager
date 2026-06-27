06f7f2e36e19c240f85883ee9197e08297db021b
Petr <peterpankratc@gmail.com>
2026-06-27 22:18:54 +0300

    add create_db funcs

---
+++ src/db/create_db.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DSN = os.getenv('DSN')
+
+class Base(DeclarativeBase):
+    pass
+
+engine = create_engine(url=DSN)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def create_db():
+    Base.metadata.create_all(bind=engine)
